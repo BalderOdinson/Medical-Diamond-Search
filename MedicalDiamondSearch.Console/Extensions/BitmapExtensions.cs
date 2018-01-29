6fddd1b3741e53a08f749beaa6a1ccc45b8878da
Dorian Brmbota <dorianb2@hotmail.com>
2018-01-29 13:13:25 +0100

    Fixed Console project

--- MedicalDiamondSearch.Console/Extensions/BitmapExtensions.cs
+++ MedicalDiamondSearch.Console/Extensions/BitmapExtensions.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.Net.Mime;
 using System.Text;
+using System.Threading.Tasks;
 using MedicalDiamondSearch.Core.Helpers;
+using MedicalDiamondSearch.Core.Settings;
+using Image = System.Drawing.Image;
 
 namespace MedicalDiamondSearch.Console.Extensions
 {
@@ -10,13 +15,48 @@ namespace MedicalDiamondSearch.Console.Extensions
     {
         public static IEnumerable<Pixel> GetPixels(this Bitmap bitmap)
         {
-            for (int y = 0; y < bitmap.Height; y++)
+            Pixel[] result = null;
+            unsafe
             {
-                for (int x = 0; x < bitmap.Width; x++)
+                BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadOnly, bitmap.PixelFormat);
+
+                int bytesPerPixel = Image.GetPixelFormatSize(bitmap.PixelFormat) / 8;
+                int heightInPixels = bitmapData.Height;
+                int widthInBytes = bitmapData.Width * bytesPerPixel;
+                int widthInPixels = bitmapData.Width;
+                byte* ptrFirstPixel = (byte*)bitmapData.Scan0;
+                result = new Pixel[heightInPixels * widthInPixels];
+
+                Parallel.For(0, heightInPixels, new ParallelOptions { MaxDegreeOfParallelism = MedicalDiamondSearchSettings.NumberOfThreads / 2 }, y =>
                 {
-                    yield return new Pixel(new Point(x,y), bitmap.GetPixel(x,y));
+                    byte* currentLine = ptrFirstPixel + (y * bitmapData.Stride);
+                    for (int x = 0; x < widthInBytes; x = x + bytesPerPixel)
+                    {
+                        lock (result)
+                        {
+                            result[y * widthInPixels + x / bytesPerPixel] = new Pixel(new Point(x / bytesPerPixel, y), Color.FromArgb(currentLine[x + 2], currentLine[x + 1], currentLine[x]));
+                        }
+                    }
+                });
+                bitmap.UnlockBits(bitmapData);
+            }
+            return result;
+        }
+
+        public static decimal Compare(this Bitmap first, Bitmap second)
+        {
+            var diff = 0;
+            for (var y = 0; y < first.Height; y++)
+            {
+                for (var x = 0; x < first.Width; x++)
+                {
+                    if (first.GetPixel(x, y) != second.GetPixel(x, y))
+                        diff++;
                 }
             }
+
+            return (decimal)diff / (first.Height * first.Width);
         }
     }
 }
+
--- MedicalDiamondSearch.Console/Program.cs
+++ MedicalDiamondSearch.Console/Program.cs
@@ -1,8 +1,15 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
+using System.Net.Mime;
+using System.Threading.Tasks;
 using MedicalDiamondSearch.Console.Extensions;
 using MedicalDiamondSearch.Core;
+using MedicalDiamondSearch.Core.Helpers;
 using MedicalDiamondSearch.Core.Settings;
 using Image = MedicalDiamondSearch.Core.Helpers.Image;
 
@@ -24,27 +31,66 @@ namespace MedicalDiamondSearch.Console
             MedicalDiamondSearchSettings.InitialTreshold = double.Parse(args[4]);
             MedicalDiamondSearchSettings.NumberOfThreads = int.Parse(args[5]);
 
+            var stopwatch = Stopwatch.StartNew();
+
+            System.Console.WriteLine("Loading images...");
+
+            var resultImage = new Bitmap(refrentImage.Width, refrentImage.Height, PixelFormat.Format32bppRgb);
+            Image cImage = null;
+            var cImageTask = Task.Run(() =>
+            {
+                cImage = new Image(currentImage.GetPixels(), currentImage.Width, currentImage.Height);
+                cImage.GenerateBlocks();
+            });
             var rImage = new Image(refrentImage.GetPixels(), refrentImage.Width, refrentImage.Height);
             rImage.GenerateBlocks();
-            var cImage = new Image(currentImage.GetPixels(), currentImage.Width, currentImage.Height);
-            cImage.GenerateBlocks();
-            var stopwatch = Stopwatch.StartNew();
-            var result = Mds.CalculateVectors(rImage, cImage);
-            stopwatch.Stop();
-            System.Console.WriteLine($"Time elapsed: {stopwatch.Elapsed.TotalSeconds} seconds");
+            cImageTask.Wait();
+
+            IDictionary<Point, Vector> result = null;
+
+
+            System.Console.WriteLine("Executing Medical Diamond Search...");
+            result = Mds.CalculateVectors(rImage, cImage);
+
+            System.Console.WriteLine("Calculating output image...");
+            var errorCount = 0;
             foreach (var vector in result)
             {
                 if (vector.Value.X != 0 || vector.Value.Y != 0)
                 {
+                    System.Console.WriteLine(
+                        $"Block({vector.Key.X},{vector.Key.Y}), vector({vector.Value.X},{vector.Value.Y})");
                     foreach (var pixel in rImage.Blocks[vector.Key].Pixels)
                     {
-                        refrentImage.SetPixel(pixel.Position.X + vector.Value.X, pixel.Position.Y + vector.Value.Y, pixel.Color);
+                        if (cImage.Pixels[
+                                new System.Drawing.Point(pixel.Position.X + vector.Value.X,
+                                    pixel.Position.Y + vector.Value.Y)].Color != pixel.Color)
+                            errorCount++;
+                        resultImage.SetPixel(pixel.Position.X + vector.Value.X, pixel.Position.Y + vector.Value.Y,
+                            pixel.Color);
+                        refrentImage.SetPixel(pixel.Position.X + vector.Value.X, pixel.Position.Y + vector.Value.Y,
+                            pixel.Color);
                     }
                 }
             }
-            refrentImage.Save("output.png");
-            System.Console.WriteLine($"Broj blokova je: {result.Count}");
-            System.Console.ReadLine();
+            System.Console.WriteLine("Saving output image...");
+            if (!Directory.Exists("output"))
+                Directory.CreateDirectory("output");
+            var output = $"output/{Guid.NewGuid().ToString()}.png";
+            var output1 = $"output/{Guid.NewGuid().ToString()}.png";
+            resultImage.Save(output);
+            refrentImage.Save(output1);
+
+            var motionError = (decimal)errorCount / (result.Count * MedicalDiamondSearchSettings.BlockSize * MedicalDiamondSearchSettings.BlockSize);
+
+            System.Console.WriteLine("Calculating relative error...");
+            var error = refrentImage.Compare(currentImage);
+
+            stopwatch.Stop();
+
+            System.Console.WriteLine($"Time elapsed: {stopwatch.Elapsed.TotalSeconds} seconds");
+            System.Console.WriteLine($"Motion relative error: {motionError.ToString("P")}");
+            System.Console.WriteLine($"Output relative error: {error.ToString("P")}");
         }
     }
 }
